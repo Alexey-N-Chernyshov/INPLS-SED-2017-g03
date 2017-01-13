e40eafd738782733671d8d1bddded80795bb8317
Alexey Chernyshov <Alexey.N.Chernyshov@gmail.com>
2017-01-13 17:12:26 +0300

    test

--- lu.uni.teaching.il2.tutorial.java.group3/src/lu/uni/teaching/il2/tutorial/java/group3/MyNewClass.java
+++ lu.uni.teaching.il2.tutorial.java.group3/src/lu/uni/teaching/il2/tutorial/java/group3/MyNewClass.java
@@ -3,8 +3,7 @@ package lu.uni.teaching.il2.tutorial.java.group3;
 public class MyNewClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub help
 	}
 
 }
