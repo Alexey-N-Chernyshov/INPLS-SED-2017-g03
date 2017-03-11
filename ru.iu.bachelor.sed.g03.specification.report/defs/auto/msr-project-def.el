1b2d37283e0001f45b8bc1e2760038df2bdd9ace
Mitrx <dmitryalxv@yandex.com>
2017-03-11 03:37:16 +0300

    added specification project files from sprint 2 submission version

---
+++ ru.iu.bachelor.sed.g03.specification.report/defs/auto/msr-project-def.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "msr-project-def"
+ (lambda ()
+   (TeX-add-symbols
+    "msrprojectname"
+    "msrReportAuthors"
+    "msrReportAffiliation"
+    "msrReportTitle"))
+ :latex)
+
