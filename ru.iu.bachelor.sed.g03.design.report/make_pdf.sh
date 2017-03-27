cd108a6ec205efc56295e204d55f50c99aa4711c
Mitrx <dmitryalxv@yandex.com>
2017-03-27 12:41:21 +0300

    created scripts for manual pdf build and cleaned up user manual

---
+++ ru.iu.bachelor.sed.g03.design.report/make_pdf.sh
@@ -0,0 +1,9 @@
+rm design.pdf
+latex design.tex
+bibtex design
+makeglossaries design
+latex design.tex
+latex design.tex
+dvips design.dvi
+ps2pdf design.ps
+rm design.aux design.dvi design.glg design.glo design.gls design.glsdefs design.idx design.lof design.maf design.mtc design.mtc0 design.out design.ps design.toc design.xdy design.bbl design.blg *.log *.lol
--- ru.iu.bachelor.sed.g03.usermanual.report/compile.sh
+++
@@ -1,5 +0,0 @@
-latex user-manual.tex
-makeglossaries user-manual
-dvips user-manual.dvi
-ps2pdf user-manual.ps
-#rm user-manual.aux user-manual.dvi user-manual.glg user-manual.glo user-manual.gls user-manual.glsdefs user-manual.idx user-manual.lof user-manual.log user-manual.maf user-manual.mtc user-manual.mtc0 user-manual.out user-manual.ps user-manual.toc user-manual.xdy
